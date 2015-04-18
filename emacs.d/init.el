a60dbeceabe7f37e9be886d587cde0ffc1aac06c
Jan Beinke <git@janbeinke.com>
2015-04-18 15:20:04 +0200

    init.el; add global theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -50,3 +50,20 @@
 (add-hook
 	'before-save-hook 'time-stamp
 )
+
+;;;; global theme
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (ample-zen)))
+ '(custom-safe-themes
+	 (quote
+		("1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
