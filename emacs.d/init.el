b9d5fdc523a94e3ec97a0d7efd057089d7753bc2
Jan Beinke <git@janbeinke.com>
2016-01-26 22:03:12 +0100

    Move theme loading to its own config file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,6 +17,8 @@
 
 (package-initialize)
 
+(config 'theme)
+
 ;;;; macros
 
 ;;;; global settings
@@ -33,25 +35,8 @@
  )
 
 (add-hook
-	'before-save-hook 'time-stamp
-)
+ 'before-save-hook 'time-stamp)
 
-;;;; global theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (ample-zen)))
- '(custom-safe-themes
-	 (quote
-		("1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;;;; global key bindings
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
