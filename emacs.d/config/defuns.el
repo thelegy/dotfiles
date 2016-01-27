c77ab79186a4f5bb0aa290911b0691da1b9d6ba4
Jan Beinke <git@janbeinke.com>
2016-01-27 08:16:21 +0100

    Add defun for automatic installation of packages

--- emacs.d/config/defuns.el
+++ emacs.d/config/defuns.el
@@ -7,3 +7,13 @@
    'noerror
    )
   )
+
+;; Function for installing and configuring packages
+(defun package (pkg)
+  (if (not (package-installed-p pkg))
+      (package-install pkg)
+    )
+  (if (package-installed-p pkg)
+      (config pkg)
+    )
+  )
