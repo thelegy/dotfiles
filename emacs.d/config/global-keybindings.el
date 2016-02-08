afde71c51b4baf3bbdebea833f552abff253c68f
Jan Beinke <git@janbeinke.com>
2016-02-08 09:50:02 +0100

    Add windmove global keybindings

--- emacs.d/config/global-keybindings.el
+++ emacs.d/config/global-keybindings.el
@@ -4,3 +4,8 @@
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+(global-set-key (kbd "M-<left>") 'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
+(global-set-key (kbd "M-<up>") 'windmove-up)
+(global-set-key (kbd "M-<down>") 'windmove-down)
