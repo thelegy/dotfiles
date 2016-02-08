c9ae475ebb96850c3f5bab6c9ad56811ba2edc3d
Jan Beinke <git@janbeinke.com>
2016-02-08 09:41:39 +0100

    Remove eval-after-load hook from smart-tabs config

--- emacs.d/config/smart-tabs-mode.el
+++ emacs.d/config/smart-tabs-mode.el
@@ -1,9 +1,8 @@
 ;;; package configuration: smart-tabs-mode
-(with-eval-after-load "smart-tabs-mode-autoloads"
-	(setq-default indent-tabs-mode nil)
-	(smart-tabs-insinuate
-   'c
-   'c++
-   'javascript
-   )
-  )
+
+(setq-default indent-tabs-mode nil)
+(smart-tabs-insinuate
+ 'c
+ 'c++
+ 'javascript
+ )
