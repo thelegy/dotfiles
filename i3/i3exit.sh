b4046e3e1a4c0c37cbb4abe6b955e2bae5543c80
Jan Beinke <git@janbeinke.com>
2016-10-07 12:56:45 +0200

    Make i3lock use a static background color again

--- i3/i3exit.sh
+++ i3/i3exit.sh
@@ -2,66 +2,53 @@
 
 function lock()
 {
-    local tmpscreenshot="/tmp/i3lock_screenshot.png"
-    if hash import && hash convert; then
-        import -window root "${tmpscreenshot}"
-        convert "${tmpscreenshot}" \
-                -resize 25% \
-                -gaussian-blur 4 \
-                -resize 400% \
-                "${tmpscreenshot}"
-        if ! i3lock -defi "${tmpscreenshot}"; then
-            i3lock -i "${tmpscreenshot}"
-        fi
-    else
-        if ! i3lock -defc 003B65; then
-            i3lock -c 003B65
-        fi
-    fi
-    return
+  if ! i3lock --show-failed-attempts --ignore-empty-password --color=202020; then
+    i3lock -ccolor=202020
+  fi
+  return
 }
 
 function logout()
 {
-    i3-msg exit
-    return
+  i3-msg exit
+  return
 }
 
 function reboot()
 {
-    systemctl reboot
-    return
+  systemctl reboot
+  return
 }
 
 function shutdown()
 {
-    systemctl poweroff
-    return
+  systemctl poweroff
+  return
 }
 
 function usage()
 {
-    echo "$0 (lock|logout|reboot|shutdown)"
-    return
+  echo "$0 (lock|logout|reboot|shutdown)"
+  return
 }
 
 case "$1" in
-    lock)
-        lock
-        ;;
-    logout)
-        logout
-        ;;
-    reboot)
-        reboot
-        ;;
-    shutdown)
-        shutdown
-        ;;
-    *)
-        usage
-        exit 2
-        ;;
+  lock)
+    lock
+    ;;
+  logout)
+    logout
+    ;;
+  reboot)
+    reboot
+    ;;
+  shutdown)
+    shutdown
+    ;;
+  *)
+    usage
+    exit 2
+    ;;
 esac
 
 exit 0
