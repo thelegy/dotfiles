7049667a70fd49ae45bae2e3c221b6d148a94da3
Jan Beinke <git@janbeinke.com>
2017-07-26 20:07:48 +0200

    Move i3 config into the XDG_CONFIG_HOME

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ d                                       ${XDG_CONFIG_HOME:-.config}
 l config/cdm                            ${XDG_CONFIG_HOME:-.config}/cdm
 l config/compton.conf                   ${XDG_CONFIG_HOME:-.config}/compton.conf
 l config/git                            ${XDG_CONFIG_HOME:-.config}/git
+l i3                                    ${XDG_CONFIG_HOME:-.config}/i3
 l config/j4status                       ${XDG_CONFIG_HOME:-.config}/j4status
 l config/redshift.conf                  ${XDG_CONFIG_HOME:-.config}/redshift.conf
 l config/sway                           ${XDG_CONFIG_HOME:-.config}/sway
@@ -19,8 +20,6 @@ l emacs.d/init.el                       .emacs.d/init.el
 d                                       .gnupg
 l gnupg/gpg.conf                        .gnupg/gpg.conf
 l gnupg/gpg-agent.conf                  .gnupg/gpg-agent.conf
-
-l i3                                    .i3
 
 l prompt                                .prompt
 
